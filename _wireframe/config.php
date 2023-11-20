4173fd02cb1b66c6058593e5219e39be54e13b5d
Richard O <73641163+rindo789@users.noreply.github.com>
2023-11-20 10:13:51 +0100

    init dashboard

--- _wireframe/config.php
+++ _wireframe/config.php
@@ -1,4 +1,11 @@
 <?php
 
 $config = [];
-$config['rewriteBase'] = '/github/bladeerp-docs/_wireframe';
+$config['mainTemplate'] = 'desktop';
+$config['skin'] = '';
+$config['rewriteBase'] = '/bladeerp-docs/_wireframe';
+
+$config['sidebarItems'] = [
+  "contacts" => ["text" => "Kontakty", 'icon' => 'fas fa-home'],
+  '' => ['text' => 'Domov', 'icon' => 'fas fa-home'],
+];
