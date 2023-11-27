d810b509eea4350d8d2129225cf13d1ceac918f0
Richard O <73641163+rindo789@users.noreply.github.com>
2023-11-27 21:02:39 +0100

    Modal Form Card responsivity, new Views

--- _wireframe/config.php
+++ _wireframe/config.php
@@ -7,5 +7,7 @@ $config['rewriteBase'] = '/bladeerp-docs/_wireframe';
 
 $config['sidebarItems'] = [
   '' => ['text' => 'Domov', 'icon' => 'fas fa-home', 'cssStyle' => 'margin-left:10px;' ],
-  "contacts" => ["text" => "Kontakty", 'icon' => 'fas fa-home', 'cssStyle' => 'margin-left:10px;' ],
+  "contacts" => ["text" => "Kontakty", 'icon' => 'fas fa-phone', 'cssStyle' => 'margin-left:10px;' ],
+  "countries" => ["text" => "Krajiny", 'icon' => 'fas fa-globe', 'cssStyle' => 'margin-left:10px;' ],
+  "categories" => ["text" => "Kategorie", 'icon' => 'fas fa-address-book', 'cssStyle' => 'margin-left:10px;' ],
 ];
