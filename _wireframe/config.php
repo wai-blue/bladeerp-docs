e75772c905b22c2bfe8b2cc1cc6f10590c1935f8
Richard O <73641163+rindo789@users.noreply.github.com>
2023-12-07 17:08:07 +0100

    ItemsSelling, Items & Packages

--- _wireframe/config.php
+++ _wireframe/config.php
@@ -10,4 +10,7 @@ $config['sidebarItems'] = [
   "contacts" => ["text" => "Kontakty", 'icon' => 'fas fa-phone', 'cssStyle' => 'margin-left:10px;' ],
   "countries" => ["text" => "Krajiny", 'icon' => 'fas fa-globe', 'cssStyle' => 'margin-left:10px;' ],
   "categories" => ["text" => "Kategorie", 'icon' => 'fas fa-address-book', 'cssStyle' => 'margin-left:10px;' ],
+  "itemSellings" => ["text" => "Sklad", 'icon' => 'fas fa-warehouse', 'cssStyle' => 'margin-left:10px;' ],
+  "items" => ["text" => "Predmety", 'icon' => 'fas fa-pallet', 'cssStyle' => 'margin-left:10px;' ],
+  "packages" => ["text" => "Balíky", 'icon' => 'fas fa-box', 'cssStyle' => 'margin-left:10px;' ],
 ];
