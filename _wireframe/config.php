f8399a361e15e18952e5f0a3dcf1c67a5ebca935
Richard O <73641163+rindo789@users.noreply.github.com>
2023-12-08 11:58:37 +0100

    Deleted shadows, Added codeList Views, Added Small Adios Modals

--- _wireframe/config.php
+++ _wireframe/config.php
@@ -11,6 +11,15 @@ $config['sidebarItems'] = [
   "countries" => ["text" => "Krajiny", 'icon' => 'fas fa-globe', 'cssStyle' => 'margin-left:10px;' ],
   "categories" => ["text" => "Kategorie", 'icon' => 'fas fa-address-book', 'cssStyle' => 'margin-left:10px;' ],
   "itemSellings" => ["text" => "Sklad", 'icon' => 'fas fa-warehouse', 'cssStyle' => 'margin-left:10px;' ],
-  "items" => ["text" => "Predmety", 'icon' => 'fas fa-pallet', 'cssStyle' => 'margin-left:10px;' ],
+  "items" => ["text" => "Tovar", 'icon' => 'fas fa-pallet', 'cssStyle' => 'margin-left:10px;' ],
   "packages" => ["text" => "Balíky", 'icon' => 'fas fa-box', 'cssStyle' => 'margin-left:10px;' ],
+  "mediaTypes" => ["text" => "Typy médií", 'icon' => 'fas fa-file-alt', 'cssStyle' => 'margin-left:10px;' ],
+  "media" => ["text" => "Média", 'icon' => 'fas fa-image', 'cssStyle' => 'margin-left:10px;' ],
+  "units" => ["text" => "Jednotky", 'icon' => 'fas fa-ruler', 'cssStyle' => 'margin-left:10px;' ],
+];
+
+$config['globalUnits'] = [
+  "units" => ['KG', 'LB', 'L', 'GAL', 'SQM', 'SQFT', 'CBM', 'CBFT'],
+  "packageUnits" => ['EA', 'CS', 'PLT', 'CTN', 'ROL', 'PPH',],
+  "mediaTypes" => ['Video', 'Photo', 'Script'],
 ];
--- _wireframe/index.js
+++ _wireframe/index.js
@@ -1,3 +1,21 @@
-function add_contactCompany() {
-  window_render('contacts/addCompanySide');
+function add_country() {
+  window_render('countries/addOrEditCountry');
+}
+function add_category() {
+  window_render('categories/addOrEditCategory');
+}
+function add_item() {
+  window_render('warehouse/addOrEditItem');
+}
+function add_package() {
+  window_render('warehouse/addOrEditPackage');
+}
+function add_media_type() {
+  window_render('codeList/addOrEditMediaType');
+}
+function add_media() {
+  window_render('codeList/addOrEditMedia');
+}
+function add_unit() {
+  window_render('codeList/addOrEditUnit');
 }
